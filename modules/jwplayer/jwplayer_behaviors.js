3603effecf104f28705ba2fd737de436e6c36994
joe <eojthebrave@gmail.com>
2011-04-25 23:48:40 -0500

    Rename filefield_jwplayer module to jwplayer.

---
+++ modules/jwplayer/jwplayer_behaviors.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Javascript for adding JWPlayer to the page.
+ *
+ * File is intentially named jwplayer_behaviors.js instead of following the
+ * standard naming convention of jwplayer.js to avoid confusion between this
+ * file and the jwplayer.js embedding library that comes with the JWPlayer
+ * software.
+ */
+Drupal.behaviors.jwplayer = function(context) {
+  var config = Drupal.settings.jwplayer;
+
+  for (var field in config) {
+    if ($('#' + field).length > 0) {
+      $player = jwplayer(field).setup(config[field]);
+      // Is error logging enabled?
+      if (Drupal.settings.jwplayer.error_log && Drupal.settings.ajaxlog.key && Drupal.settings.ajaxlog.token) {
+        $player.onError(Drupal.jwplayer.JWPlayerLog);
+      }
+    }
+  }
+}
+
+Drupal.jwplayer = {};
+Drupal.jwplayer.JWPlayerLog = function(e) {
+  Drupal.ajaxlog('jwplayer', e.message);
+}
