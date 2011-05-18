805eea0285ea87c7b67fa8482071fc8be74b7a19
joe <eojthebrave@gmail.com>
2011-05-18 15:07:39 -0500

    Better handling of free_video field on video grid views.

---
+++ themes/videola_omega/templates/views-view-field--field-videola-free-value.tpl.php
@@ -0,0 +1,19 @@
+<?php
+ /**
+  * Variables available:
+  * - $view: The view object
+  * - $field: The field handler object that can process the input
+  * - $row: The raw SQL result that can be used
+  * - $output: The processed output that will normally be used.
+  *
+  * When fetching output from the $row, this construct should be used:
+  * $data = $row->{$field->field_alias}
+  *
+  * The above will guarantee that you'll always get the correct data,
+  * regardless of any changes in the aliasing that might happen if
+  * the view is modified.
+  */
+?>
+<?php if ($row->{$field->field_alias} == 1): ?>
+  <a class="free-video-badge-small" href="<?php print url('node/'. $row->nid); ?>"></a>
+<?php endif; ?>
